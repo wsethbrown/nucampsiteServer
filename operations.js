c2ab86d8b77e676ab15665cd9ee947e9c53bab89
Seth <wsethbrown@gmail.com>
2021-06-24 11:03:34 -0400

    add promise handling

---
+++ operations.js
@@ -0,0 +1,21 @@
+
+//Insert Document into Database
+exports.insertDocument = (db, document, collection, ) => {
+    const coll = db.collection(collection)
+    return coll.insertOne(document)
+}
+
+exports.findDocuments = (db, collection, ) => {
+    const coll = db.collection(collection)
+    return coll.find().toArray()
+}
+
+exports.removeDocument = (db, document, collection, ) => {
+    const coll = db.collection(collection)
+    return coll.deleteOne(document)
+}
+
+exports.updateDocument = (db, document, update, collection, ) => {
+    const coll = db.collection(collection)
+    return coll.updateOne(document, { $set: update }, null)
+}
